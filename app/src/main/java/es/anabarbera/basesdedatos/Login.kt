10224efdbd11466fcbfafa66648b9d8da39e6fe9
Ana B <anabarbera90@gmail.com>
2024-02-06 19:56:32 +0100

    conectamos los archivos login y registro con la Firebase

--- app/src/main/java/es/anabarbera/basesdedatos/Login.kt
+++ app/src/main/java/es/anabarbera/basesdedatos/Login.kt
@@ -1,15 +1,21 @@
 package es.anabarbera.basesdedatos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
 import android.widget.EditText
 import android.widget.ProgressBar
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
     private lateinit var editTextUser:EditText
     private lateinit var editTextPsswd:EditText
     private lateinit var progressBarLogin:ProgressBar
 
+   private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -20,5 +26,37 @@ class Login : AppCompatActivity() {
         editTextPsswd=findViewById(R.id.editTextPsswd)
         progressBarLogin=findViewById(R.id.progressBarLogin)
 
+        auth = FirebaseAuth.getInstance()
+
+    }
+    fun forgotPassword(view: View) {
+
+    }
+    fun registrar(view:View) {
+        startActivity(Intent(this,Registro::class.java))
+    }
+    fun login(view:View) {
+        loginUser()
+    }
+
+    private fun loginUser(){
+
+        val user:String = editTextUser.text.toString()
+        val password:String = editTextPsswd.text.toString()
+
+        if(!TextUtils.isEmpty(user)  && !TextUtils.isEmpty(password)){
+            progressBarLogin.visibility = View.VISIBLE
+            auth.signInWithEmailAndPassword(user,password).addOnCompleteListener(this){
+                    task->
+                if (task.isSuccessful) {
+                    action()
+                }else{
+                    Toast.makeText(this,"Error en la autenticación",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+    private fun action(){
+        startActivity(Intent(this,MainActivity::class.java))
     }
 }
--- app/src/main/java/es/anabarbera/basesdedatos/Registro.kt
+++ app/src/main/java/es/anabarbera/basesdedatos/Registro.kt
@@ -1,10 +1,17 @@
 package es.anabarbera.basesdedatos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
 import android.widget.EditText
 import android.widget.ProgressBar
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.pdatabase.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class Registro : AppCompatActivity() {
 
@@ -15,8 +22,8 @@ class Registro : AppCompatActivity() {
     private lateinit var registroContrasena:EditText
     private lateinit var progressBar:ProgressBar
 
-  //  private lateinit var dbReference:DatabaseReference
-    // private lateinit var database:FirebaseDatabase
+    private lateinit var dbReference:DatabaseReference
+    private lateinit var database:FirebaseDatabase
     private lateinit var auth:FirebaseAuth
 
 
@@ -30,12 +37,53 @@ class Registro : AppCompatActivity() {
 
         progressBar=findViewById(R.id.progressBar)
 
-      //  database=FirebaseDatabase.getInstance()
+        database=FirebaseDatabase.getInstance()
         auth=FirebaseAuth.getInstance()
 
-       // dbReference = database.reference.child("User")
-
-
+       dbReference = database.reference.child("User")
 
     }
+    fun registrar(view: View){
+        createNewAccount()
+    }
+
+    private fun createNewAccount(){
+        val name:String = registroNombre.text.toString()
+        val lastName:String = registroApellido.text.toString()
+        val email:String = registroEmail.text.toString()
+        val password:String = registroContrasena.text.toString()
+
+        if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(lastName) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)){
+            progressBar.visibility = View.VISIBLE
+            auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this){
+                    task->
+                if(task.isComplete){
+                    val user:FirebaseUser?=auth.currentUser
+                    verifyEmail(user)
+
+                    val userBD = dbReference.child(user!!.uid)
+                    userBD.child("Name").setValue(name)
+                    userBD.child("Apellido").setValue(lastName)
+                    action()
+                }
+            }
+
+        }
+    }
+    private fun action(){
+        startActivity(Intent(this,Login::class.java))
+    }
+    private fun verifyEmail(user:FirebaseUser?){
+        user?.sendEmailVerification()
+            ?.addOnCompleteListener(this){
+                    task->
+                if(task.isComplete) {
+                    Toast.makeText(this, "Email enviado",Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    Toast.makeText(this, "Error al enviar el email",Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
 }
